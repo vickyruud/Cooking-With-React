eb0afc4bdd12b4af5ffcdefcfd6cdb6765868da1
vickyruud <aprajit.k@gmail.com>
2022-04-27 21:21:27 -0400

    add useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,53 @@
-import React, { useState } from "react";
-import RecipeList from "./RecipeList";
+import React, { useState, useEffect } from 'react';
+import RecipeList from './RecipeList'
 import '../css/app.css'
 import { v4 as uuidv4 } from 'uuid';
 
-
-export const RecipeContext = React.createContext();
+export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes'
 
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes)
 
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
 
-  const [recipes, setRecipes] = useState(sampleRecipes);
-
-  const handleRecipeAdd = () => {
-  const newRecipe = {
-    id: uuidv4(),
-    name: 'New',
-    Servigs: 1,
-    cookTime: '1:00',
-    instructions: 'steps',
-    ingredients: [
-      {
-        id: uuidv4(),
-        name: 'Name',
-        amount: '1Tbsp'
-      }
-    ]
-  }
-    setRecipes([...recipes, newRecipe]);
-  }
-  
-  const handleRecipeDelete = (id) => {
-    setRecipes(recipes.filter(recipe => recipe.id !== id))
-  }
-  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
 
   const recipeContextValue = {
     handleRecipeAdd,
     handleRecipeDelete
   }
 
-  
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        { id: uuidv4(), name: 'Name', amount: '1 Tbs' }
+      ]
+    }
+
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
 
   return (
     <RecipeContext.Provider value={recipeContextValue}>
-      <RecipeList
-        recipes={recipes}
-        
-      />
+      <RecipeList recipes={recipes} />
     </RecipeContext.Provider>
   )
 }
-
-
 
 const sampleRecipes = [
   {
@@ -59,7 +55,7 @@ const sampleRecipes = [
     name: 'Plain Chicken',
     servings: 3,
     cookTime: '1:45',
-    instructions: "1. Put Salt on Chicken\n2. Put Chicken in oven\n3.Eat Chicken",
+    instructions: "1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken",
     ingredients: [
       {
         id: 1,
@@ -71,17 +67,15 @@ const sampleRecipes = [
         name: 'Salt',
         amount: '1 Tbs'
       }
-      
     ]
-  
   },
-   {
+  {
     id: 2,
     name: 'Plain Pork',
     servings: 5,
     cookTime: '0:45',
-     instructions: "1. Put Paprika on Pork\n2. Put Pork in oven\n3.Eat Pork",
-      ingredients: [
+    instructions: "1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork",
+    ingredients: [
       {
         id: 1,
         name: 'Pork',
@@ -92,9 +86,7 @@ const sampleRecipes = [
         name: 'Paprika',
         amount: '2 Tbs'
       }
-      
     ]
-  
   }
 ]
 
